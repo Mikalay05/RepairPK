98ee87db15d936187d4289b32f4d7ea8df867528
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-17 00:52:42 +0300

    feat: description Appointment model

--- RepairPK/Models/Appointment.cs
+++ RepairPK/Models/Appointment.cs
@@ -2,5 +2,10 @@
 {
     public class Appointment
     {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } = null!;
+        public string Content { get; set; }
+        public DateTime AppointmentDate { get; set; }
     }
 }
