9548c84fd97ddd72e384ffec2bf447f675bbc943
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-17 00:45:44 +0300

    feat: description Repair model

--- RepairPK/Models/Repair.cs
+++ RepairPK/Models/Repair.cs
@@ -2,5 +2,15 @@
 {
     public class Repair
     {
+        public int Id { get; set; }
+        public int HardwareId { get; set; }
+        public Hardware Hardware { get; set; } = null!;
+        public DateTime RepairDate { get; set; }
+        public string Description { get; set; }
+        public decimal Cost { get; set; }
+        public int PartId { get; set; }
+        public Part Part { get; set; }
+        public int CountPart { get; set; }
+
     }
 }
