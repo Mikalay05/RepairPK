28989eb6b6c84606e0838a26b03e015befebe213
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-17 00:38:04 +0300

    feat: description Part model

--- RepairPK/Models/Part.cs
+++ RepairPK/Models/Part.cs
@@ -2,5 +2,14 @@
 {
     public class Part
     {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int QuantityAvailable { get; set; }
+
+        public decimal Price { get; set; }
+
+        public ICollection<Repair> Repairs { get; } = new List<Repair>();
     }
 }
