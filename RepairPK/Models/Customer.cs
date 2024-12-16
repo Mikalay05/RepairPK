cea9f851b9c29eeb639f65cbf1bfee8bdb762565
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-17 00:40:26 +0300

    feat: description Customer model

--- RepairPK/Models/Customer.cs
+++ RepairPK/Models/Customer.cs
@@ -2,5 +2,11 @@
 {
     public class Customer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public ICollection<Feedback> Feedbacks { get; } = new List<Feedback>();
+        public ICollection<Appointment> Appointments { get; } = new List<Appointment>();
     }
 }
