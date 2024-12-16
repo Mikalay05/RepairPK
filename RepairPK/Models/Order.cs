5b8750cb3987954094c10a661dc60e85981ea5d9
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-17 00:51:21 +0300

    feat: description Order model

--- RepairPK/Models/Order.cs
+++ RepairPK/Models/Order.cs
@@ -2,5 +2,13 @@
 {
     public class Order
     {
+        public int Id { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime CompletionDate { get; set; }
+        public bool PaymentStatus { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } = null!;
+
+        public ICollection<Hardware> Hardwares  { get; } = new List<Hardware>();
     }
 }
