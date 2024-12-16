8a4c6d2ed313d3a4308f4ca0fff6c94f37151220
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-17 00:42:13 +0300

    feat: description Feedback model

--- RepairPK/Models/Feedback.cs
+++ RepairPK/Models/Feedback.cs
@@ -2,5 +2,12 @@
 {
     public class Feedback
     {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } = null!;
+
+        public short Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
