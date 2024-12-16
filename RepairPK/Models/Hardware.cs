7794e85a0ef8a7ff6162545894e41dcd7a2cf3fe
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-17 00:48:43 +0300

    faet: description Hardware model

--- RepairPK/Models/Hardware.cs
+++ RepairPK/Models/Hardware.cs
@@ -2,5 +2,14 @@
 {
     public class Hardware
     {
+        public int Id { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string SerialNumber { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; } = null!;
+        public string Type { get; set; }
+
+        public ICollection<Repair> Repairs = new List<Repair>();
     }
 }
