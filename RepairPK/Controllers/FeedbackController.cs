67b50558ba2f5dac99c412653a869932c77726e3
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-17 21:29:24 +0300

    feat: add FeedbackController and add getAll request

---
+++ RepairPK/Controllers/FeedbackController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RepairPK.Contracts;
+
+namespace RepairPK.Controllers
+{
+    [ApiController]
+    [Route("api/feedback")]
+    public class FeedbackController: ControllerBase
+    {
+        private readonly IFeedbackRepository _feedbackRepository;
+        public FeedbackController(IFeedbackRepository feedbackRepository)
+        {
+            _feedbackRepository = feedbackRepository;
+        }
+        [HttpGet]
+        public IActionResult GetFeedbacks()
+        {
+            var feedbacks = _feedbackRepository.GetAllFeedbacks(trackChanges: true);
+
+            return Ok(feedbacks);
+        }
+
+    }
+}
