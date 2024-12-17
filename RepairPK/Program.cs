89d7e919df526d6708ea0580957c31b3f29feb73
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-17 23:24:07 +0300

    feat: instart swagger

--- RepairPK/Program.cs
+++ RepairPK/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using RepairPK.Contracts;
 using RepairPK.Repository;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace RepairPK
 {
@@ -16,6 +20,12 @@ namespace RepairPK
             var connectionString = builder.Configuration.GetConnectionString("MySQLinDB");
             builder.Services.AddDbContextPool<RepositoryContext>(
                 options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Repair API", Version = "v1" });
+            });
 
             builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
             builder.Services.AddScoped<IPartRepository, PartRepository>();
@@ -34,10 +44,19 @@ namespace RepairPK
             }
             app.UseStaticFiles();
 
-            app.MapControllers();
+            app.UseRouting(); // Обязательно добавьте этот вызов
+
             app.UseAuthorization();
 
+            // Настройка Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Repair API V1");
+                options.RoutePrefix = string.Empty; // Доступ к Swagger UI по корневому URL
+            });
 
+            app.MapControllers();
             app.Run();
         }
     }
