319b7fda63c20654583730e7d731f818d787bee9
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-17 21:25:19 +0300

    feat: description gets in Repair repository

--- RepairPK/Dto/RepairDto.cs
+++ RepairPK/Dto/RepairDto.cs
@@ -3,12 +3,10 @@
     public record RepairDto(
         int Id,
         int HardwareId,
-        string HardwareType,
         DateTime RepairDate,
         string Description,
         decimal Cost,
         int? PartId,
-        string? PartName,
         int? CountPart
     );
 }
--- RepairPK/Repository/RepairRepository.cs
+++ RepairPK/Repository/RepairRepository.cs
@@ -1,10 +1,48 @@
 ﻿using RepairPK.Contracts;
+using RepairPK.Dto;
 using RepairPK.Models;
 
 namespace RepairPK.Repository
 {
     public class RepairRepository : RepositoryBase<Repair>, IRepairRepository
     {
+        public RepairRepository(RepositoryContext context) : base(context) { }
+        public IEnumerable<RepairDto> GetAllRepairs(bool trachChanges)
+        {
+            var repairs = FindAll(trachChanges)
+                .OrderBy(r => r.Id)
+                .ToList();
 
+            var repairsDto = repairs.Select(r =>
+
+                new RepairDto(
+                    r.Id,
+                    r.HardwareId,
+                    r.RepairDate,
+                    r.Description,
+                    r.Cost,
+                    r.PartId,
+                    r.CountPart
+                    ))
+                .ToList();
+
+            return repairsDto;
+        }
+        public RepairDto GetRepair(int id, bool trachChanges)
+        {
+            var repair = FindByCondition(r => r.Id.Equals(id), trachChanges)
+                .SingleOrDefault();
+
+            var repairDto = new RepairDto(
+                    repair.Id,
+                    repair.HardwareId,
+                    repair.RepairDate,
+                    repair.Description,
+                    repair.Cost,
+                    repair.PartId,
+                    repair.CountPart
+                );
+            return repairDto;
+        }
     }
 }
