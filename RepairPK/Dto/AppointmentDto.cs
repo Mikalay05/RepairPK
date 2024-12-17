9fb96b71f9aa78cfdfbec71f71f78300e3737f8e
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-17 21:10:16 +0300

    feat: description Appointment Repository

--- RepairPK/Dto/AppointmentDto.cs
+++ RepairPK/Dto/AppointmentDto.cs
@@ -3,7 +3,6 @@
     public record AppointmentDto(
         int Id,
         int CustomerId,
-        string CustomerName,
         string Content,
         DateTime AppointmentDate
     );
--- RepairPK/Repository/AppointmentRepository.cs
+++ RepairPK/Repository/AppointmentRepository.cs
@@ -1,9 +1,32 @@
 ﻿using RepairPK.Contracts;
+using RepairPK.Dto;
 using RepairPK.Models;
 
 namespace RepairPK.Repository
 {
     public class AppointmentRepository : RepositoryBase<Appointment>, IAppointmentRepository
     {
+        public AppointmentRepository(RepositoryContext context) : base(context) { }
+        public IEnumerable<AppointmentDto> GetAllAppointments(bool trachChanges)
+        {
+            var appointments = FindAll(trachChanges)
+                .OrderBy(c => c.Id)
+                .ToList();
+
+            var appointmentsDto = appointments.Select(a =>
+
+                new AppointmentDto(a.Id, a.CustomerId,  a.Content, a.AppointmentDate))
+                .ToList();
+
+            return appointmentsDto;
+        }
+        public AppointmentDto GetAppointment(int id, bool trachChanges)
+        {
+            var appointment = FindByCondition(a => a.Id.Equals(id), trachChanges)
+                .SingleOrDefault();
+
+            var appointmentDto = new AppointmentDto(appointment.Id, appointment.CustomerId, appointment.Content, appointment.AppointmentDate);
+            return appointmentDto;
+        }
     }
 }
